60b86887718ca63365d5c2af4bfb7e7dcdb0c77a
SophYo <sophiya.dimitrova@gmail.com>
2019-07-25 21:50:47 +0300

    initial commit with some code in

--- src/blackBoxInteger/Main.java
+++ src/blackBoxInteger/Main.java
@@ -1,7 +1,35 @@
 package blackBoxInteger;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String input;
+
+        try {
+            Constructor constructor = BlackBoxInt.class.getDeclaredConstructor();
+
+            constructor.setAccessible(true);
+
+            constructor.newInstance()
+
+        } catch (NoSuchMethodException |
+                IllegalAccessException |
+                InstantiationException |
+                InvocationTargetException exception) {
+
+            exception.printStackTrace();
+
+        }
+
+        while (!"end".equalsIgnoreCase(input = scanner.nextLine())) {
+
+
+
+        }
     }
 }
