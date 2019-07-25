8e9e2d92925631402007fe307b8149783af19773
SophYo <sophiya.dimitrova@gmail.com>
2019-07-25 20:20:29 +0300

    switch added

--- src/harvestingFields/Main.java
+++ src/harvestingFields/Main.java
@@ -1,7 +1,37 @@
 package harvestingFields;
 
+import java.lang.reflect.Field;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		String input = scanner.nextLine();
+
+		Field[] fields = RichSoilLand.class.getDeclaredFields();
+
+		while (!input.equals("HARVEST")) {
+
+			switch (input) {
+				case "private ":
+
+					break;
+				case "protected ":
+
+					break;
+				case "public ":
+
+					break;
+				case "all ":
+
+					break;
+
+
+			}
+
+			input = scanner.nextLine();
+		}
 
 	}
 }
