64629a6682d0db0a1924cf670e689d94ac232c68
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 13:05:33 +0300

    added HorseMan

---
+++ src/barracksWars/models/units/HorseMan.java
@@ -0,0 +1,7 @@
+package barracksWars.models.units;
+
+public class HorseMan extends AbstractUnit {
+    public HorseMan(int health, int attackDamage) {
+        super(health, attackDamage);
+    }
+}
