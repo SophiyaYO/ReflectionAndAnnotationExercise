a97c2cc5437f6939338cb935a7409441c46739be
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 15:46:22 +0300

    optimizing imports and removing interpretCommand() to CommandInterpreter

--- src/barracksWars/core/Engine.java
+++ src/barracksWars/core/Engine.java
@@ -1,14 +1,12 @@
 package barracksWars.core;
 
 
-import barracksWars.interfaces.*;
+import barracksWars.interfaces.CommandInterpreter;
 import barracksWars.interfaces.Runnable;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 public class Engine implements Runnable {
     private static final String COMMANDS_PACKAGE_PATH = "barracksWars.core.commands.";
@@ -48,32 +46,5 @@ public class Engine implements Runnable {
         }
     }
 
-    private String interpretCommand(String[] data, String commandName) {
-        String result = "Invalid Command!";
-
-        commandName = Character.toUpperCase(commandName.charAt(0)) +
-                commandName.substring(1);
-
-        try {
-            Class<? extends Executable> clazz = (Class<? extends Executable>)
-                    Class.forName(COMMANDS_PACKAGE_PATH + commandName);
-
-            Constructor<? extends Executable> constructor =
-                    clazz.getDeclaredConstructor(
-                            String[].class, Repository.class, UnitFactory.class);
-
-            constructor.setAccessible(true);
-
-            Executable executable = constructor.newInstance
-                    (data, this.repository, this.unitFactory);
-
-            result = executable.execute();
-
-        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-        }
-
-        return result;
-    }
 
 }
