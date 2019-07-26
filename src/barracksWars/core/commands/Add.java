39de957feca3675977cf5b58ddb73ff267fbf7a9
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 14:14:10 +0300

    inlined command

--- src/barracksWars/core/commands/Add.java
+++ src/barracksWars/core/commands/Add.java
@@ -15,7 +15,6 @@ public class Add extends Command {
         String unitType = this.getData()[1];
         Unit unitToAdd = this.getUnitFactory().createUnit(unitType);
         this.getRepository().addUnit(unitToAdd);
-        String output = unitType + " added!";
-        return output;
+        return unitType + " added!";
     }
 }
