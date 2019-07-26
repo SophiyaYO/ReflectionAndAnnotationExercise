c9a201305925dd1641fc7f7016849d141f1535cf
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 15:56:19 +0300

    const added; interpretCommand changes (not final)

--- src/barracksWars/core/commands/CommandInterpreterImpl.java
+++ src/barracksWars/core/commands/CommandInterpreterImpl.java
@@ -5,7 +5,12 @@ import barracksWars.interfaces.Executable;
 import barracksWars.interfaces.Repository;
 import barracksWars.interfaces.UnitFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class CommandInterpreterImpl implements CommandInterpreter {
+    private static final String COMMANDS_PACKAGE_PATH = "barracksWars.core.commands.";
+
     private Repository repository;
     private UnitFactory unitFactory;
 
@@ -16,6 +21,33 @@ public class CommandInterpreterImpl implements CommandInterpreter {
 
     @Override
     public Executable interpretCommand(String[] data, String commandName) {
-        return null;
+        Executable executable = null;
+
+        commandName = Character.toUpperCase(commandName.charAt(0)) +
+                commandName.substring(1);
+
+        try {
+            Class<? extends Executable> clazz = (Class<? extends Executable>)
+                    Class.forName(COMMANDS_PACKAGE_PATH + commandName);
+
+            Constructor<? extends Executable> constructor =
+                    clazz.getDeclaredConstructor(
+                            String[].class, Repository.class, UnitFactory.class);
+
+            constructor.setAccessible(true);
+
+            executable = constructor.newInstance
+                    (data, this.repository, this.unitFactory);
+
+        } catch (ClassNotFoundException
+                | NoSuchMethodException
+                | InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException e) {
+
+            e.printStackTrace();
+        }
+
+        return executable;
     }
 }
