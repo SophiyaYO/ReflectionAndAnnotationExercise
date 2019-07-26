86fa2a1b85759c3dbf3326ddf7cfbe9d332d8cf4
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 14:09:05 +0300

    constructor became public

--- src/barracksWars/core/commands/Command.java
+++ src/barracksWars/core/commands/Command.java
@@ -2,7 +2,6 @@ package barracksWars.core.commands;
 
 import barracksWars.interfaces.Executable;
 import barracksWars.interfaces.Repository;
-import barracksWars.interfaces.Unit;
 import barracksWars.interfaces.UnitFactory;
 
 public abstract class Command implements Executable {
@@ -10,7 +9,7 @@ public abstract class Command implements Executable {
    private Repository repository;
    private UnitFactory unitFactory;
 
-    protected Command(String[] data, Repository repository, UnitFactory unitFactory) {
+    public Command(String[] data, Repository repository, UnitFactory unitFactory) {
         this.data = data;
         this.repository = repository;
         this.unitFactory = unitFactory;
