0962a286da5a5409286bea38cd185a6c7c8aba4a
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 13:11:18 +0300

    constructor access modifier changed to public

--- src/barracksWars/models/units/Gunner.java
+++ src/barracksWars/models/units/Gunner.java
@@ -4,7 +4,7 @@ public class Gunner extends AbstractUnit {
     private static final int GUNNER_HEALTH = 20;
     private static final int GUNNER_DAMAGE = 20;
 
-    protected Gunner() {
+    public Gunner() {
         super(GUNNER_HEALTH, GUNNER_DAMAGE);
     }
 }
