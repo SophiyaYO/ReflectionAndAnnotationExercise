11dad55650977cc73751000c3b6abe5baad569e5
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 15:38:38 +0300

    added

---
+++ src/barracksWars/core/commands/CommandInterpreterImpl.java
@@ -0,0 +1,14 @@
+package barracksWars.core.commands;
+
+import barracksWars.interfaces.CommandInterpreter;
+import barracksWars.interfaces.Executable;
+
+public class CommandInterpreterImpl implements CommandInterpreter {
+
+
+
+    @Override
+    public Executable interpretCommand(String[] data, String commandName) {
+        return null;
+    }
+}
