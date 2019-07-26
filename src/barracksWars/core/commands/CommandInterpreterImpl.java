b04be75ec1fd6664624f7a9dc7e70e96688e3378
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 15:43:53 +0300

    constructor and fields added

--- src/barracksWars/core/commands/CommandInterpreterImpl.java
+++ src/barracksWars/core/commands/CommandInterpreterImpl.java
@@ -2,10 +2,17 @@ package barracksWars.core.commands;
 
 import barracksWars.interfaces.CommandInterpreter;
 import barracksWars.interfaces.Executable;
+import barracksWars.interfaces.Repository;
+import barracksWars.interfaces.UnitFactory;
 
 public class CommandInterpreterImpl implements CommandInterpreter {
+    private Repository repository;
+    private UnitFactory unitFactory;
 
-
+    public CommandInterpreterImpl(Repository repository, UnitFactory unitFactory) {
+        this.repository = repository;
+        this.unitFactory = unitFactory;
+    }
 
     @Override
     public Executable interpretCommand(String[] data, String commandName) {
