749e26a83a5a9fbe364f27655760eca087426126
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 15:26:16 +0300

    put all except data under command ( as said in the description of pr 05)

--- src/barracksWars/core/commands/Command.java
+++ src/barracksWars/core/commands/Command.java
@@ -6,24 +6,28 @@ import barracksWars.interfaces.UnitFactory;
 
 public abstract class Command implements Executable {
    private String[] data;
-   private Repository repository;
-   private UnitFactory unitFactory;
+//   private Repository repository;
+//   private UnitFactory unitFactory;
 
-    public Command(String[] data, Repository repository, UnitFactory unitFactory) {
+//    public Command(String[] data, Repository repository, UnitFactory unitFactory) {
+//        this.data = data;
+//        this.repository = repository;
+//        this.unitFactory = unitFactory;
+//    }
+
+    public Command(String[] data) {
         this.data = data;
-        this.repository = repository;
-        this.unitFactory = unitFactory;
     }
 
     protected String[] getData() {
         return this.data;
     }
 
-    protected Repository getRepository() {
-        return this.repository;
-    }
+//    protected Repository getRepository() {
+//        return this.repository;
+//    }
 
-    protected UnitFactory getUnitFactory() {
-        return this.unitFactory;
-    }
+//    protected UnitFactory getUnitFactory() {
+//        return this.unitFactory;
+//    }
 }
